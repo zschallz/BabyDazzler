fe19381825b6387549abc4678eae365c068fe7fc
Zachary Zimmerman <zschallz@indiana.edu>
2011-10-16 00:39:54 -0400

    Dumping project thus far:
    1. WPF "MainWindow" that includes a black canvas that has one test Ellipse drawn on it.
    2. An unimplemented skeleton class which will be instantiated and hand off random shapes on a StackPanel.

---
+++ BabyDazzler/Dazzlers/VisualDazzleObj.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Shapes;
+using System.Windows.Controls;
+
+namespace BabyDazzler.Dazzlers
+{
+    class VisualDazzleObj
+    {
+        private Shape shape;
+
+        public VisualDazzleObj()
+        {
+            this.shape = randomShape();
+        }
+
+        private Shape randomShape()
+        {
+            Random random = new Random();
+            int randomNum = random.Next(0, 2);
+
+            switch (randomNum)
+            {
+                case 0:
+                    return buildEllipse();
+                case 1:
+                    return buildRectangle();
+                case 2:
+                    return buildTriangle();
+                default:
+                    throw new 
+                        InvalidOperationException("Expected randomNum to be between 0 and 2, was " + randomNum);
+            }
+        }
+
+        private Polygon buildTriangle()
+        {
+            throw new NotImplementedException();
+        }
+
+        private Rectangle buildRectangle()
+        {
+            throw new NotImplementedException();
+        }
+
+        private Ellipse buildEllipse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public StackPanel GetView()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BabyDazzler/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BabyDazzler
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DrawShape();
+        }
+
+        public void DrawShape()
+        {
+            // Test code
+
+            // Create a StackPanel to contain the shape.
+            StackPanel myStackPanel = new StackPanel();
+
+            // Create a red Ellipse.
+            Ellipse myEllipse = new Ellipse();
+
+            // Create a SolidColorBrush with a red color to fill the 
+            // Ellipse with.
+            SolidColorBrush mySolidColorBrush = new SolidColorBrush();
+
+            // Describes the brush's color using RGB values. 
+            // Each value has a range of 0-255.
+            mySolidColorBrush.Color = Color.FromArgb(255, 255, 255, 0);
+            myEllipse.Fill = mySolidColorBrush;
+            myEllipse.StrokeThickness = 2;
+            myEllipse.Stroke = Brushes.White;
+
+            // Set the width and height of the Ellipse.
+            myEllipse.Width = 200;
+            myEllipse.Height = 100;
+
+            // Add the Ellipse to the StackPanel.
+            myStackPanel.Children.Add(myEllipse);
+
+            WindowGrid.Children.Add(myStackPanel);
+        }
+    }
+}
